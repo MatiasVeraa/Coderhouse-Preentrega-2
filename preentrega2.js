3649801e5bbbe1f3475bf34ae3da8302b9834a1e
Matias <matias@digitalarts.com.ar>
2024-12-22 19:11:05 -0300

    Primer commit: agregar archivos

---
+++ preentrega2.js
@@ -0,0 +1,12 @@
+
+
+
+
+// Crea el elemento <h1>
+let h1 = document.createElement('h1');
+
+// Establece el texto dentro del <h1>
+h1.textContent = 'Este es un título H2';
+
+// Agrega el <h1> al cuerpo de la página
+document.body.appendChild(h1);
